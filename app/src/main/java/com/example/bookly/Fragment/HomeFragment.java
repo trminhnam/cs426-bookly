d11e196fb1746882af5cc193ee92048af93759dc
trminhnam <trminhnam20082002@gmail.com>
2022-08-26 12:03:13 +0700

    Add dashboard

--- app/src/main/java/com/example/bookly/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/bookly/Fragment/HomeFragment.java
@@ -6,8 +6,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.bookly.Adapter.StoryAdapter;
+import com.example.bookly.Model.StoryModel;
 import com.example.bookly.R;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,50 +22,92 @@ import com.example.bookly.R;
  */
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    RecyclerView storyRv;
+    ArrayList<StoryModel> storyList;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        storyRv = view.findViewById(R.id.storyRecycleView);
+
+        storyList = new ArrayList<>();
+        storyList.add(new StoryModel(
+                R.drawable.resource_default,
+                R.drawable.bg_gradient,
+                R.drawable.ic_baseline_person_24,
+                "User Name 1"));
+
+        storyList.add(new StoryModel(
+                R.drawable.resource_default,
+                R.drawable.bg_gradient,
+                R.drawable.ic_baseline_person_24,
+                "User Name 2"));
+
+        storyList.add(new StoryModel(
+                R.drawable.resource_default,
+                R.drawable.bg_gradient,
+                R.drawable.ic_baseline_person_24,
+                "User Name 3"));
+
+        storyList.add(new StoryModel(
+                R.drawable.resource_default,
+                R.drawable.bg_gradient,
+                R.drawable.ic_baseline_person_24,
+                "User Name 4"));
+
+        storyList.add(new StoryModel(
+                R.drawable.resource_default,
+                R.drawable.bg_gradient,
+                R.drawable.ic_baseline_person_24,
+                "User Name 5"));
+
+        StoryAdapter adapter = new StoryAdapter(storyList, getContext());
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        storyRv.setLayoutManager(linearLayoutManager);
+        storyRv.setNestedScrollingEnabled(false);
+
+        storyRv.setAdapter(adapter);
+
+        return view;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
