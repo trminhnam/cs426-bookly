2b12131a1ca4fd1010e16a549a8ad625b1fcccaf
trminhnam <trminhnam20082002@gmail.com>
2022-08-26 16:27:35 +0700

    Update home fragment with dashboard recycler view

--- app/src/main/java/com/example/bookly/MainActivity.java
+++ app/src/main/java/com/example/bookly/MainActivity.java
@@ -24,29 +24,6 @@ public class MainActivity extends AppCompatActivity {
     private static BottomNavigationView navigation;
 
 
-
-//    private Fragment getCurrentFragment(int index) {
-//        switch (index) {
-//            case 0: // HomeFragment
-//                fragment = HomeFragment.getInstance();
-//                break;
-//            case 1: // Notification
-//                fragment = NotificationFragment.getInstance();
-//                break;
-//            case 2: // game
-//                fragment = AddPostFragment.getInstance();
-//                break;
-//            case 3: // shopping cart
-//                fragment = SearchFragment.getInstance();
-//                break;
-//            case 4: // map
-//                fragment = ProfileFragment.getInstance();
-//                break;
-//        }
-//        return fragment;
-//    }
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -69,27 +46,22 @@ public class MainActivity extends AppCompatActivity {
                 switch (item.getItemId()) {
                     case R.id.navigation_home:
                         transaction.replace(R.id.frame_layout_container, new HomeFragment());
-                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
                         transaction.commit();
                         return true;
                     case R.id.navigation_notification:
                         transaction.replace(R.id.frame_layout_container, new NotificationFragment());
-                        Toast.makeText(MainActivity.this, "Notification", Toast.LENGTH_SHORT).show();
                         transaction.commit();
                         return true;
                     case R.id.navigation_addpost:
                         transaction.replace(R.id.frame_layout_container, new AddPostFragment());
-                        Toast.makeText(MainActivity.this, "addpost", Toast.LENGTH_SHORT).show();
                         transaction.commit();
                         return true;
                     case R.id.navigation_search:
                         transaction.replace(R.id.frame_layout_container, new SearchFragment());
-                        Toast.makeText(MainActivity.this, "search", Toast.LENGTH_SHORT).show();
                         transaction.commit();
                         return true;
                     case R.id.navigation_profile:
                         transaction.replace(R.id.frame_layout_container, new ProfileFragment());
-                        Toast.makeText(MainActivity.this, "user", Toast.LENGTH_SHORT).show();
                         transaction.commit();
                         return true;
                 }
@@ -97,10 +69,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-//
-//    private void switchFragment(int index) {
-//        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-//        transaction.replace(R.id.frame_container, getCurrentFragment(index));
-//        transaction.commit();
-//    }
 }
