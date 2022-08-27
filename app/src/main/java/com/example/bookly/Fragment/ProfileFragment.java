cf264099f2de3855517aa3cadb0431f094bab18d
thanhduycao <thanhduycao1202@gmail.com>
2022-08-27 08:59:45 +0700

    add toolbar but still have bug

--- app/src/main/java/com/example/bookly/Fragment/ProfileFragment.java
+++ app/src/main/java/com/example/bookly/Fragment/ProfileFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
